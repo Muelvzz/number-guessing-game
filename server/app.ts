6d5bb3751dae77f9ab0c5b4576e081c8a9498ce7
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-09-12 21:06:26 +0800

    feat: implemented user inputs for the game difficulty

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,16 @@
-let message: string = "Hello, TypeScript!";
-console.log(message);
+import askUserDifficulty from "./utils/user/askUserDifficulty.js"
+
+let numOfGuesses: number = 5
+
+console.log("============================================")
+console.log("----- NUMBER GUESSING GAME -----\n")
+
+console.log("Welcome to the Number Guessing Game")
+console.log("I'm thinking of a number between 1 and 100,")
+console.log(`You have ${numOfGuesses} chances to guess the correct number\n`)
+
+console.log("Please select the difficulty level:")
+console.log("1. Easy (10 chances)")
+console.log("2. Medium (5 chances)")
+console.log("3. Hard (3 chances)\n")
+askUserDifficulty()
---
+++ server/utils/user/askUserDifficulty.ts
@@ -0,0 +1,25 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+enum gameDifficulty {
+  Easy = 1,
+  Medium = 2,
+  Hard = 3
+}
+
+export default async function askUserDifficulty(): Promise<number | void> {
+  const rl = readline.createInterface({ input, output })
+  
+  try {
+    while (true) {
+      let gameMode: number = parseInt(await rl.question("Enter your choice: "))
+      if (gameMode in gameDifficulty) {
+        console.log(`Great! You have selected ${gameDifficulty[gameMode]}`)
+        return gameMode
+      }
+      console.log("\nInvalid input, please try again.")
+    }
+  } finally {
+    rl.close()
+  }
+}
